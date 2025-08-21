c40b5155dc4cd6121bcc2d30221bd53be67ee269
manishbeth18 <manish.beth@gmail.com>
2025-08-21 19:10:37 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Square {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for the square of it.");
+        int n = sc.nextInt();
+        
+        int d=n*n;
+        
+        System.out.println("The square of the given  number is "+d);
+        
+    }
+
+}
